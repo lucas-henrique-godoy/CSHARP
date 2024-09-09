a34dfd0adb822ec381c729af1901e97236d20264
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-09-09 17:02:03 -0300

    COPIEI E COLEI OS ARQUIVOS AQUI

---
+++ EstruturasDecisao/EstruturasDecisao/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace EstruturasDecisao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "Debugando C";
+            int idade = 18;
+            float decibeis = 15.14f;
+            double salario = 10.50;
+            bool verdade = true;
+            var nsei = 0; // variavel que não espera nenhum valor, mas identifica qual é o valor que esta vindo.
+
+            if (nome == "Lucas" && idade == 18)
+            {
+                Console.WriteLine(nome);
+            }
+            else if (nome == "Debugando C")
+            {
+                Console.WriteLine("Ola Mundo!");
+            }
+            else
+            {
+                Console.WriteLine("Sair");// Pode adicionar algum código ou remover se não necessário
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Meu_Primeiro_Hello_World/Meu_Primeiro_Hello_World/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Meu_Primeiro_Hello_World
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
