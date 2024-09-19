8a1c9191e3c5fda17683d2879d6d00f1ef2c634f
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-09-19 10:10:33 -0300

    testando commit novamente

--- Meu_Primeiro_Hello_World/Meu_Primeiro_Hello_World/Program.cs
+++ Meu_Primeiro_Hello_World/Meu_Primeiro_Hello_World/Program.cs
@@ -1,12 +1,12 @@
-﻿using System;
+﻿using System; // Usando a biblioteca system
 
-namespace Meu_Primeiro_Hello_World
+namespace Meu_Primeiro_Hello_World // Namespace permite referenciar em outros projetos o local que queremos acessar.
 {
-    internal class Program
+    internal class Program // Classe interna que não consegue ser vista externamente.
     {
-        static void Main(string[] args)
+        static void Main(string[] args) // Método estático que sempre vai se construir independente da instanciação da classe, para podermos iniciar o programa.
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!"); // Console(terminal) - WriteLine(escreva linha)
         }
     }
 }
--- Meu_Primeiro_Hello_World/Meu_Primeiro_Hello_World/obj/Debug/net5.0/Meu_Primeiro_Hello_World.AssemblyInfo.cs
+++ Meu_Primeiro_Hello_World/Meu_Primeiro_Hello_World/obj/Debug/net5.0/Meu_Primeiro_Hello_World.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Meu_Primeiro_Hello_World")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0e1442c61b41d784b7e113002bd607b4ee46eb5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cfcfd64d2c67c23f7f771ab19633460c0213721")]
 [assembly: System.Reflection.AssemblyProductAttribute("Meu_Primeiro_Hello_World")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Meu_Primeiro_Hello_World")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
