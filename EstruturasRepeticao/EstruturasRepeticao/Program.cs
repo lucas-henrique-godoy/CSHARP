0900c9a7fb0a4da255c728741792d6930f58902a
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-09-20 14:49:38 -0300

    criei o EstruturasRepeticao

--- EstruturasRepeticao/EstruturasRepeticao/Program.cs
+++ EstruturasRepeticao/EstruturasRepeticao/Program.cs
@@ -10,7 +10,23 @@ namespace EstruturasRepeticao
     {
         static void Main(string[] args)
         {
+            string nome = "Debugando ";
+            int idade = 18;
+            float decibeis = 15.14f;
+            double salario = 10.50;
+            bool verdade = true;
+            var nsei = 0;
 
+
+            // Estruturas de Repetição
+
+            for (int i = 0; i < 5;i++)
+            {
+                Console.WriteLine(i);                
+            }
+
+            Console.WriteLine("FIM!");
+
+            Console.ReadKey();
         }
-    }
-}
+}   }
