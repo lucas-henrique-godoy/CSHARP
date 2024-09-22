7072fc99f769e1f50a6421e4d0d129357151eeee
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-09-21 23:28:50 -0300

    abri o EstruturasRepeticao

--- Meu_Primeiro_Hello_World/Meu_Primeiro_Hello_World/obj/Debug/net5.0/Meu_Primeiro_Hello_World.AssemblyInfo.cs
+++ Meu_Primeiro_Hello_World/Meu_Primeiro_Hello_World/obj/Debug/net5.0/Meu_Primeiro_Hello_World.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Meu_Primeiro_Hello_World")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88bfea0e78c637567e44bf9d86da38f80f107dcb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e360767d06fdaf7911cc2b1350bf94f3f8bebd55")]
 [assembly: System.Reflection.AssemblyProductAttribute("Meu_Primeiro_Hello_World")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Meu_Primeiro_Hello_World")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Meu_Primeiro_Hello_World/Meu_Primeiro_Hello_World/obj/Release/net5.0/Meu_Primeiro_Hello_World.AssemblyInfo.cs
+++ Meu_Primeiro_Hello_World/Meu_Primeiro_Hello_World/obj/Release/net5.0/Meu_Primeiro_Hello_World.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Meu_Primeiro_Hello_World")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71e1f0c47d2a8dba609e4fa0340780ad0a0a511c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e360767d06fdaf7911cc2b1350bf94f3f8bebd55")]
 [assembly: System.Reflection.AssemblyProductAttribute("Meu_Primeiro_Hello_World")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Meu_Primeiro_Hello_World")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
