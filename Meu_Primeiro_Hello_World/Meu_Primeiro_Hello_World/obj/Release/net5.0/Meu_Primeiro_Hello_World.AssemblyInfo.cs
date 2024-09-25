94dde23f0480642c857d240cecffcddf29223b5f
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-09-25 09:21:40 -0300

    Dentro do Namespace EstruturasRepeticao eu criei uma classe Endereco.

--- Meu_Primeiro_Hello_World/Meu_Primeiro_Hello_World/obj/Release/net5.0/Meu_Primeiro_Hello_World.AssemblyInfo.cs
+++ Meu_Primeiro_Hello_World/Meu_Primeiro_Hello_World/obj/Release/net5.0/Meu_Primeiro_Hello_World.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Meu_Primeiro_Hello_World")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4484fddaecb0931f40fbc11cbf5eaab16f9691f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fd41863c0a20fa4cf9ec6d3031578b0a8623afe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Meu_Primeiro_Hello_World")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Meu_Primeiro_Hello_World")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
