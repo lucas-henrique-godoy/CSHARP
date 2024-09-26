df4ca03b966e8bc7c4a07aa47ee7d47838a2310c
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-09-26 08:06:06 -0300

    Acrescentei uma explicação sobre o código  da classe Endereco.

--- EstruturasDecisao/EstruturasDecisao/Endereco.cs
+++ EstruturasDecisao/EstruturasDecisao/Endereco.cs
@@ -17,3 +17,10 @@ namespace EstruturasDecisao
         }
     }
 }
+
+/*
+                    EXPLICAÇÃO DO CÓDIGO ACIMA:
+
+ O código define um namespace chamado EstruturasDecisao e uma classe pública Endereco, que representa um endereço. A classe possui uma propriedade Logradouro, que pode ser lida publicamente, mas só alterada internamente. Além disso, inclui um método público chamado AlterarEndereco, que recebe um parâmetro logradouro, atualiza a propriedade Logradouro com esse valor e retorna o novo valor da propriedade. Dessa forma, a classe encapsula a funcionalidade relacionada ao endereço, permitindo a alteração controlada do logradouro.
+ 
+ */
--- Meu_Primeiro_Hello_World/Meu_Primeiro_Hello_World/obj/Release/net5.0/Meu_Primeiro_Hello_World.AssemblyInfo.cs
+++ Meu_Primeiro_Hello_World/Meu_Primeiro_Hello_World/obj/Release/net5.0/Meu_Primeiro_Hello_World.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Meu_Primeiro_Hello_World")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5327d3b565afeabec78b80cafb8022cc20de28cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+430cd7596086dded8c44a9235b7d1cd46f44e9bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Meu_Primeiro_Hello_World")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Meu_Primeiro_Hello_World")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
