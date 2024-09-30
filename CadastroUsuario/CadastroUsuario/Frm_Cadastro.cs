d426258d69d32ebf83d866068a39e0870226289f
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-09-29 21:51:41 -0300

    criei o Frm_Cadastro no windows forms

--- CadastroUsuario/CadastroUsuario/Frm_Cadastro.cs
+++ CadastroUsuario/CadastroUsuario/Frm_Cadastro.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace CadastroUsuario
 {
-    public partial class Form1 : Form
+    public partial class Frm_Cadastro : Form
     {
-        public Form1()
+        public Frm_Cadastro()
         {
             InitializeComponent();
         }
